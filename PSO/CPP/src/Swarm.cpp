78ff4e38d093a2e43ee442ebb71047e2dd673411
Amin Sedaghat <amin32846@gmail.com>
2024-03-13 03:37:30 +0000

    Added Swarm CPP

---
+++ PSO/CPP/src/Swarm.cpp
@@ -0,0 +1,50 @@
+#include "Swarm.h"
+#include <iostream>
+
+Swarm::Swarm(size_t size, double searchSpaceMin, double searchSpaceMax, std::function<double(double, double)> objectiveFunc)
+    : searchSpaceMin(searchSpaceMin), searchSpaceMax(searchSpaceMax), objectiveFunc(objectiveFunc), dis(searchSpaceMin, searchSpaceMax) {
+    std::random_device rd;
+    gen.seed(rd());
+    for (size_t i = 0; i < size; ++i) {
+        particles.emplace_back(Particle(randomDouble(), randomDouble()));
+    }
+}
+
+void Swarm::initialize() {
+    for (auto& particle : particles) {
+        particle.evaluateBestPosition(objectiveFunc);
+        updateGlobalBest();
+    }
+}
+
+void Swarm::optimize(int maxIterations) {
+    const double inertiaWeight = 0.729, cognitiveWeight = 1.49445, socialWeight = 1.49445;
+
+    for (int iter = 0; iter < maxIterations; ++iter) {
+        for (auto& particle : particles) {
+            double randP = randomDouble(), randG = randomDouble();
+            particle.updateVelocity(globalBestPosition, inertiaWeight, cognitiveWeight, socialWeight, randP, randG);
+            particle.updatePosition();
+            particle.evaluateBestPosition(objectiveFunc);
+            updateGlobalBest();
+        }
+    }
+}
+
+void Swarm::printGlobalBest() const {
+    std::cout << "Global Best Position: (" << globalBestPosition.first << ", " << globalBestPosition.second << ")\n";
+    std::cout << "Global Best Value: " << globalBestValue << std::endl;
+}
+
+void Swarm::updateGlobalBest() {
+    for (auto& particle : particles) {
+        if (particle.getBestValue() < globalBestValue) {
+            globalBestValue = particle.getBestValue();
+            globalBestPosition = particle.getBestPosition();
+        }
+    }
+}
+
+double Swarm::randomDouble() {
+    return dis(gen);
+}
