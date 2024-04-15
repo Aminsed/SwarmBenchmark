2b29d7da1d3826906bf3b28a9c47432c3a891a26
Amin Sedaghat <amin32846@gmail.com>
2024-04-15 15:45:03 -0400

    re-structure for TSP

--- ACO/CUDA/ObjectiveFunction.cuh
+++ ACO/CUDA/ObjectiveFunction.cuh
@@ -31,6 +31,7 @@ __device__ double objectiveFunction(double* position) {
 }
 */
 
+
 /*
 // Griewank function
 #define DIMENSIONS 3 // Number of dimensions in the optimization problem
@@ -42,7 +43,7 @@ __device__ double objectiveFunction(double* position) {
 }
 */
 
-/*
+
 // Schaffer function N.4
 #define DIMENSIONS 4 // Number of dimensions in the optimization problem
 __device__ double objectiveFunction(double* position) {
@@ -52,72 +53,6 @@ __device__ double objectiveFunction(double* position) {
     double denominator = pow(1.0 + 0.001 * (x * x + y * y), 2);
     return 0.5 + numerator / denominator;
 }
-*/
 
-// TSP objective function
-#define DIMENSIONS 10 // Number of cities in the TSP problem
-__device__ double distance[DIMENSIONS][DIMENSIONS] = {
-    {0.0, 2.0, 3.0, 4.0, 5.0, 1.0, 2.0, 3.0, 4.0, 5.0},
-    {2.0, 0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 1.0, 2.0, 3.0},
-    {3.0, 1.0, 0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 1.0, 2.0},
-    {4.0, 2.0, 1.0, 0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 1.0},
-    {5.0, 3.0, 2.0, 1.0, 0.0, 1.0, 2.0, 3.0, 4.0, 5.0},
-    {1.0, 4.0, 3.0, 2.0, 1.0, 0.0, 1.0, 2.0, 3.0, 4.0},
-    {2.0, 5.0, 4.0, 3.0, 2.0, 1.0, 0.0, 1.0, 2.0, 3.0},
-    {3.0, 1.0, 5.0, 4.0, 3.0, 2.0, 1.0, 0.0, 1.0, 2.0},
-    {4.0, 2.0, 1.0, 5.0, 4.0, 3.0, 2.0, 1.0, 0.0, 1.0},
-    {5.0, 3.0, 2.0, 1.0, 5.0, 4.0, 3.0, 2.0, 1.0, 0.0}
-};
-
-struct City {
-    int index;
-    double value;
-};
-
-__device__ void sortCities(City* cities, int size) {
-    for (int i = 0; i < size - 1; i++) {
-        for (int j = 0; j < size - i - 1; j++) {
-            if (cities[j].value > cities[j + 1].value) {
-                // Swap cities
-                City temp = cities[j];
-                cities[j] = cities[j + 1];
-                cities[j + 1] = temp;
-            }        }
-    }
-}
-
-__device__ double objectiveFunction(double* position) {
-    double totalDistance = 0.0;
-    int* tour = new int[DIMENSIONS];
-    City* cities = new City[DIMENSIONS];
-    
-    // Convert position to cities
-    for (int i = 0; i < DIMENSIONS; i++) {
-        cities[i].index = i;
-        cities[i].value = position[i];
-    }
-    
-    // Sort cities based on position values
-    sortCities(cities, DIMENSIONS);
-    
-    // Create tour based on sorted cities
-    for (int i = 0; i < DIMENSIONS; i++) {
-        tour[i] = cities[i].index;
-    }
-    
-    // Calculate total distance
-    for (int i = 0; i < DIMENSIONS - 1; i++) {
-        int city1 = tour[i];
-        int city2 = tour[i + 1];
-        totalDistance += distance[city1][city2];
-    }
-    int lastCity = tour[DIMENSIONS - 1];
-    int firstCity = tour[0];
-    totalDistance += distance[lastCity][firstCity];
-    
-    delete[] tour;
-    delete[] cities;
-    return totalDistance;
-}
 
 #endif
